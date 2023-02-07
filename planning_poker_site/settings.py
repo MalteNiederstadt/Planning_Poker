d1039bf33a4b3e7509c08a6bc4b2656c7532e898
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-02-07 11:47:21 +0100

    fix

--- planning_poker_site/settings.py
+++ planning_poker_site/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = 'static/'
+STATIC_ROOT = BASE_DIR / "/static"
 
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
