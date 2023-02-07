e0b528913281f1af7fd8ddb3cabae2a8e00ccba7
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-02-07 08:39:43 +0100

    css fix

--- planning_poker_site/settings.py
+++ planning_poker_site/settings.py
@@ -143,7 +143,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
-
+STATICFILES_DIRS = (
+    #os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'planning_poker'), 
+    os.path.join(BASE_DIR, 'static', 'admin'), 
+)
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
 #     #os.path.join(BASE_DIR, 'static'),
