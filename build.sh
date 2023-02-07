cd4ce289fa37c77d7a1b6935eaffcd57bbd93aea
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-02-07 13:05:33 +0100

    fix

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py findstatic --verbosity 2 static
 
 if [ "$CREATE_SUPERUSER" = "" ]
 then
