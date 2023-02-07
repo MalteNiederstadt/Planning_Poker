eff2e3d867c68709ed2b192ef0e706a5263e37bb
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-02-07 14:09:30 +0100

    fix

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py findstatic --verbosity 2 static
 
