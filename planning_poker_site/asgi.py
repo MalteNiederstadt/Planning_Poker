a8c0d57f9895d913eb038c952f41b5431aaec657
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-02-07 08:04:27 +0100

    css fix

--- planning_poker_site/asgi.py
+++ planning_poker_site/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "planning_poker_site.settings")
-
-application = get_asgi_application()
+application = Cling(get_wsgi_application())
