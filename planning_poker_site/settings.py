3d96ea82a702cafbb54031b1ed8a5af67d2945f1
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-02-07 08:12:55 +0100

    css fix

--- planning_poker_site/settings.py
+++ planning_poker_site/settings.py
@@ -141,7 +141,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'planning_poker'), 
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
