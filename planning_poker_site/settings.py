09ac690f9c97f2a8b3b61c2730c8d28fef9770a0
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-02-07 08:21:27 +0100

    css fix

--- planning_poker_site/settings.py
+++ planning_poker_site/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'django.contrib.humanize',
-    "django.contrib.staticfiles",
     'channels',
     'planning_poker.apps.ChannelsPresenceConfig',
     'planning_poker'
@@ -142,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
