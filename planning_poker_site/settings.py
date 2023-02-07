736c73b824965f64e6d10d18c51d018059d8576f
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-02-07 08:19:32 +0100

    css fix

--- planning_poker_site/settings.py
+++ planning_poker_site/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'django.contrib.humanize',
+    "django.contrib.staticfiles",
     'channels',
     'planning_poker.apps.ChannelsPresenceConfig',
     'planning_poker'
@@ -140,14 +141,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_URL = "static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'static', 'planning_poker'), 
-)
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     #os.path.join(BASE_DIR, 'static'),
+#     os.path.join(BASE_DIR, 'static', 'planning_poker'), 
+# )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
